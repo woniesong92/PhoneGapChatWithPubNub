cce1cc8facc8c6540332dd91d7bc3281e2f788a5
Howon Song <hs459@cornell.edu>
2014-10-30 16:34:41 -0400

    weird error message from PubNub onLine

---
+++ platforms/ios/www/js/index.js
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        console.log('Received Event: ' + id);
+
+        var pubnub = PUBNUB.init({
+            channel: "chat",
+            publish_key: "pub-c-aa0da548-076b-4c46-bf87-87d2d457368a",
+            subscribe_key: "sub-c-c3f6a9fe-45cb-11e4-a251-02ee2ddab7fe"
+        });
+
+        pubnub.subscribe({
+            channel: 'chat',
+            message: handleMessage
+        });
+
+        // // Grab references for all of our elements.
+        // var messageContent = $('#messageContent'),
+        //     sendMessageButton = $('#sendMessageButton'),
+        //     messageList = $('#messageList');
+
+
+        // // Compose and send a message when the user clicks our send message button.
+        // sendMessageButton.click(function (event) {
+        //     var message = messageContent.val();
+     
+        //     if (message != '') {
+        //         pubnub.publish({
+        //             channel: 'chat',
+        //             message: {
+        //                 username: 'test',
+        //                 text: message
+        //             }
+        //         });
+     
+        //         messageContent.val("");
+        //     }
+        // });    
+     
+        // Handles all the messages coming in from pubnub.subscribe.
+        function handleMessage(message) {
+            console.log(message);
+            console.log("pubnub is working!!");
+     
+            // Scroll to bottom of page
+            // $("html, body").animate({ scrollTop: $(document).height() - $(window).height() }, 'slow');
+        };        
+
+    }
+};
+
+
+
